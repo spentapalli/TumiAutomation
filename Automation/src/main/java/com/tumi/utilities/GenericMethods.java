adbe5e1bf3c571e8f52527ba01c9707c65626a47
seleniumnov <seleniumnov10@gmail.com>
2018-11-29 23:45:28 +0530

    g

--- Automation/src/main/java/com/tumi/utilities/GenericMethods.java
+++ Automation/src/main/java/com/tumi/utilities/GenericMethods.java
@@ -35,7 +35,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
--- Automation/src/test/java/com/tumi/demotestcases/OrderWithMonogram.java
+++ Automation/src/test/java/com/tumi/demotestcases/OrderWithMonogram.java
@@ -2,13 +2,9 @@ package com.tumi.demotestcases;
 
 import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import com.tumi.dataProvider.ReadTestData;
-import com.tumi.pageObjects.MiniCartPage;
-import com.tumi.pageObjects.ShippingPage;
 import com.tumi.utilities.GenericMethods;
 
 public class OrderWithMonogram extends GenericMethods{
@@ -17,9 +13,6 @@ public class OrderWithMonogram extends GenericMethods{
 
 	@Test
 	public void testOrderWithMonogram() throws InterruptedException {
-		
-		MiniCartPage cart=PageFactory.initElements(driver, MiniCartPage.class);
-		ShippingPage shipping=PageFactory.initElements(driver, ShippingPage.class);
 		
 	}
 }
