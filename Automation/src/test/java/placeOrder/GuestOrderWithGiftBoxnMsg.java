edb1568f36eb4e9e8f549346688a3614f7101ddf
seleniumnov <seleniumnov10@gmail.com>
2019-01-23 20:45:47 +0530

    Shwetha Changes 01/23

---
+++ Automation/src/test/java/placeOrder/GuestOrderWithGiftBoxnMsg.java
@@ -0,0 +1,32 @@
+package placeOrder;
+
+import org.testng.annotations.Test;
+
+import com.tumi.reports.Reports;
+import com.tumi.utilities.GenericMethods;
+import com.tumi.utilities.TumiLibs;
+
+/**
+ * @author Shwetha Capo
+ * 
+ */
+
+public class GuestOrderWithGiftBoxnMsg extends GenericMethods{
+	/*TA-103 
+	 * Verify Order with merchandise Ready to ship + Gift Boxing + Gift Message for Guest User
+	 * */
+	
+	@Test
+	public void orderWithGiftBoxnMsgAsGuest() throws Exception {
+		TumiLibs.addProductToCart("PlaceOrder", "OrderWithGiftServices");
+		click(pdp.getAddToCart(), "Add To Cart");
+		click(minicart.getProceedCheckOut(), "Proceed to Checkout");
+		click(gift.getMakeThisGift(), "Make this Gift");
+		TumiLibs.addGiftMessage("PlaceOrder", "OrderWithGiftServices");
+		TumiLibs.addGiftBox();
+		click(gift.getContinueGiftService(), "Continue");
+		TumiLibs.completeOrder("PlaceOrder", "TumiOrder");
+
+	}
+
+}
