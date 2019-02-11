73d9cab2bcb137046a3acf9b50c8c08eae4dedc4
seleniumnov <seleniumnov10@gmail.com>
2019-02-11 09:16:27 +0530

    Suuresh: resolved process issue and removed brackes in demo.xml

--- Automation/src/main/java/com/tumi/utilities/UIFunctions.java
+++ Automation/src/main/java/com/tumi/utilities/UIFunctions.java
@@ -404,8 +404,8 @@ public class UIFunctions extends GenericMethods {
 
 	public static void completeOrder() {
 
-		delay(2000);
-		domClick(review.getPlaceOrder(), "Place Order");
+		click(review.getPlaceOrder(), "Place Order");
+		
 		do {
 			delay(2000);
 		} while (confirmation.getWithForConfirmation().isDisplayed());
@@ -416,8 +416,9 @@ public class UIFunctions extends GenericMethods {
 		}
 		orderNumber = getText(confirmation.getOrderNumber());
 		logger.log(Status.INFO, "Thank you for Your Order, here is your Order Number " + orderNumber);
-		captureOrderConfScreen("OrderConfirmation");
 		delay(3000);
+		captureOrderConfScreen("OrderConfirmation");
+		
 	}
 
 	public static void addPromotionalCodeAtCart(String sheet, String testCase) {
@@ -663,7 +664,6 @@ public class UIFunctions extends GenericMethods {
 		click(multiShip.getNext(), "Continue next shipping");
 		webclick(shipMethod.getStandardShippingMethod(), "Standard Shipping Method");
 		click(multiShip.getNextShipment(), "Continue next shipment");
-		delay(3000);
 		domClick(multiShip.getAddShippment0(), "add shipment 2");
 		if (selectedCountry.contains("Canada")){
 			addMultishipGuestDeatils(testData.get("shipmentCA2"), testData.get("nextAddressLine1"));
@@ -746,8 +746,8 @@ public class UIFunctions extends GenericMethods {
 
 		// billing page
 		input(guestBillPage.getNameOnCard(), testData.get("NameOnCard"), "Name on Card");
-
 		input(guestBillPage.getCardNumber(), testData.get("CardNumber"), "Card Number");
+		
 		if (browserName.equals("firefox")) {
 			selectMonthInFF();
 			selectYearInFF();
@@ -780,7 +780,7 @@ public class UIFunctions extends GenericMethods {
 	}
 
 	public static void addTumiStudio() {
-		click(tumiId.getTumiIdDesign(), "click on TumiID");
+		click(tumiId.getTumiIdDesign(), "TumiID");
 		delay(2000);
 		// click(tumiId.getMainBody(),"Main Body");
 		click(tumiId.getShadowGrayColor(), "Main Body in Shadow gray Color");
