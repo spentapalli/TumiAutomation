689da32555e87465b61c6f7e56f6c920c146fcee
seleniumnov <seleniumnov10@gmail.com>
2019-02-20 22:56:47 +0530

    updates

--- Automation/src/main/java/com/tumi/reports/Reports.java
+++ Automation/src/main/java/com/tumi/reports/Reports.java
@@ -298,10 +298,15 @@ public class Reports {
 	}
 
 	public static void maximizeBrowser() {
-		if (OSFinder.isWindows()) {
-			driver.manage().window().maximize();
-		} else {
-			driver.manage().window().setSize(new Dimension(1600, 900));
+		try {
+			if (OSFinder.isWindows()) {
+				driver.manage().window().maximize();
+			} else {
+				driver.manage().window().setSize(new Dimension(1600, 900));
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
 		}
 	}
 
