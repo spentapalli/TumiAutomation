2909d2a381c95ceda0b6e1c17f974df7af52d494
seleniumnov <seleniumnov10@gmail.com>
2019-02-28 20:33:00 +0530

    updates shwetha

--- Automation/src/main/java/com/tumi/webPages/PGP.java
+++ Automation/src/main/java/com/tumi/webPages/PGP.java
@@ -17,15 +17,15 @@ public class PGP extends GenericMethods {
 		Reports.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(how = How.XPATH, using = "(//a[contains(text(),'Luggage')])[1]")
 	private WebElement luggage;
 
 	public WebElement getluggage() {
 
 		return explicitWait(luggage);
-	}
-	
+	}// div[@id='product-filters']/div[2]
+
 	@FindBy(how = How.XPATH, using = "(//ul[contains(@class,'nav-sub-list')]/li/ul/li/ul/li/ul/li/a)[1]")
 	private WebElement carryOnLuggage;
 
@@ -34,47 +34,30 @@ public class PGP extends GenericMethods {
 		return carryOnLuggage;
 	}
 
-	/*
-	 * //@FindBy(how = How.XPATH, using =
-	 * "//div[@id='grid-container']/div[1]/div[2]/div[2]/div[2]/a/span/div[1]")
-	 * private WebElement SelectProduct;
-	 * 
-	 * public WebElement getSelectProduct() {
-	 * 
-	 * return SelectProduct; }
-	 */
 	@FindBy(how = How.XPATH, using = "//div[@id='grid-container']/div[1]/div[2]/div[3]/div[2]/a/span/div[1]")
 	private WebElement SelectProduct;
 
-	public  WebElement getSelectProduct() {
-		
-			return SelectProduct;
+	public WebElement getSelectProduct() {
+
+		return SelectProduct;
 	}
-	/*
-	 * @FindBy(how = How.XPATH, using =
-	 * "//div[@id='grid-container']/div[1]/div[2]/div[4]/div[2]/a/span/div[1]")
-	 * private WebElement SelectProductBag;
-	 * 
-	 * public WebElement getSelectProductBag() {
-	 * 
-	 * return SelectProductBag; }
-	 */
 
 	@FindBy(how = How.XPATH, using = "//div[@id='grid-container']/div[1]/div[2]/div[1]/div[3]/a")
 	private WebElement QuickShop;
 
-	public  WebElement getQuickShop() {
-		
-			return QuickShop;
+	public WebElement getQuickShop() {
+
+		return QuickShop;
 	}
+
 	@FindBy(how = How.XPATH, using = "(//button[@type='submit'])[2]")
 	private WebElement QuickShopAddtoCart;
 
-	public  WebElement getQuickShopAddtoCart() {
-		
-			return QuickShopAddtoCart;
+	public WebElement getQuickShopAddtoCart() {
+
+		return QuickShopAddtoCart;
 	}
-	
+
 	@FindBy(how = How.XPATH, using = "//div[@id='Color_group-ctrl']/ul/li/form/label")
 	private List<WebElement> colorsList;
 
@@ -83,6 +66,32 @@ public class PGP extends GenericMethods {
 		return colorsList;
 	}
 
-	
+	@FindBy(how = How.XPATH, using = "//div[@id='product-filters']/div[2]")
+	private WebElement productType;
+
+	public WebElement getProductType() {
+		return productType;
+	}
+
+	@FindBy(how = How.XPATH, using = "//input[@id='facet-Carry-On Luggage']")
+	private WebElement carryOnBagPT;
+
+	public WebElement getCarryOnBagPT() {
+		return explicitWait(carryOnBagPT);
+	}
+
+	@FindBy(how = How.XPATH, using = "//input[@id='facet-Carry-On Luggage']/following::span[2]")
+	private WebElement carryOnCountBagPT;
+
+	public WebElement getCarryOnCountBagPT() {
+		return carryOnCountBagPT;
+	}
+
+	@FindBy(how = How.XPATH, using = "(//span[contains(@class,'totalCount')])[2]")
+	private WebElement resultCount;
+
+	public WebElement getResultCount() {
+		return resultCount;
+	}
 
 }
