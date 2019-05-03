4783fddfc95fd51b0873268d9cf9bc4d4107897d
Suuresh <skurry@coredatalabs.com>
2019-05-03 10:07:21 +0530

    updated methods

--- Automation/src/test/java/com/tumi/testcases/ProductDetails.java
+++ Automation/src/test/java/com/tumi/testcases/ProductDetails.java
@@ -173,7 +173,7 @@ public class ProductDetails extends GenericMethods {
 				} catch (Exception e) {
 					verifypdpAsser.fail("Mini Cart is not opened when click on Add to Cart");
 				}
-			} else if (pdp.getOutOfStock().isDisplayed()) {
+			} else if (pdp.getOutofStock().isDisplayed()) {
 				verifypdpAsser.fail("Product is Out of Stock, Unable to verify Add to Cart");
 			}
 		} catch (Exception e) {
